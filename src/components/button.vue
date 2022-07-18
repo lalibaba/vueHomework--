a1f07677e08ffc8b7fe504e9ce45629345b4a7ff
ywj <3031477860@qq.com>
2022-07-18 20:49:53 +0800

    99

---
+++ src/components/button.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <button :class="type" @click="btn">{{ title }}</button>
+  </div>
+</template>
+<script>
+export default {
+  // props: ['title', 'type'],
+  props: {
+    title: String,
+    type: {
+      type: String,
+      default: 'success',
+      validator: (val) => {
+        return ['primary', 'good', 'success'].includes(val)
+      },
+    },
+  },
+  methods: {
+    btn() {
+      this.$emit('click1',)
+    },
+  },
+}
+</script>
+<style scoped>
+button {
+  text-align: center;
+  width: 110px;
+  height: 44px;
+  border-radius: 5px;
+  font-size: 20px;
+}
+.primary {
+  border: 1px solid #ccc;
+  background-color: white;
+  color: black;
+}
+.success {
+  border: 1px solid green;
+  background-color: green;
+  color: #fff;
+}
+.good {
+  border: 1px solid blue;
+  background-color: blue;
+  color: #fff;
+}
+</style>
---
+++ src/作业9封装按钮.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <elbutton title="默认按钮" @click1="btn"></elbutton>{{ a }}
+    <elbutton title="成功按钮" type="success"></elbutton>
+    <elbutton title="主要按钮" type="good"></elbutton>
+  </div>
+</template>
+
+<script>
+import elbutton from './components/button.vue'
+export default {
+  data() {
+    return {
+      a: 0,
+    }
+  },
+  components: {
+    elbutton,
+  },
+  methods: {
+    btn() {
+      console.log('你好帅')
+    },
+  },
+  mounted() {
+    this.a++
+  },
+}
+</script>
+
+<style></style>
